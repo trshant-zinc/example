e3951e29ba0fda7e81b8acfb0fcda7dc93d046c7
Trshant <trshant@gmail.com>
2015-07-16 17:14:50 +0530

    inital commit
    pehla nasha ;)

---
+++ index.php
@@ -0,0 +1,23 @@
+<?PHP
+
+
+$requested_uri = '/profile/rajesh--prabhu/';
+$pos1 = stripos($requested_uri, '/venue/');
+$pos2 = stripos($requested_uri, '/organisation/');
+$pos3 = stripos($requested_uri, '/case-study/');
+$pos4 = stripos($requested_uri, '/profile/');
+
+// removed that line!
+//
+// aawaarapan.....banjaraapann
+// adadad
+// asdadadasdads
+// adadadadasdadsasdad
+// asdadadad
+// ada
+// aawaarapan.....banjaraapanns
+// ssadasdsadasd
+//
+if( $pos1 !== false || $pos2 !== false || $pos3 !== false || $pos4 !== false ){ echo "true" ; } else { echo "false" ; }
+
+?>
