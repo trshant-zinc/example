f8e562886840ff1b81239bb5d8f2f66f910cd4d5
Trshant <trshant@gmail.com>
2015-07-16 17:27:41 +0530

    lets see this works

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ $pos4 = stripos($requested_uri, '/profile/');
 // ada
 // aawaarapan.....banjaraapanns
 // ssadasdsadasd
+// hope this works
 //
 if( $pos1 !== false || $pos2 !== false || $pos3 !== false || $pos4 !== false ){ echo "true" ; } else { echo "false" ; }
 
