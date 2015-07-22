362ca5480d93e011d520d09f12628b8655af65ca
Server <server@server.com>
2015-07-22 13:03:53 +0530

    changed stuff

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ $pos4 = stripos($requested_uri, '/profile/');
 //
 // removed that line!
 //
+// hello!
+//
+
 if( $pos1 !== false || $pos2 !== false || $pos3 !== false || $pos4 !== false ){ echo "true" ; } else { echo "false" ; }
 
 ?>
