0bafa45f4ed0105d72dc71b3007999c3a04154e1
Server <server@server.com>
2015-07-22 12:50:43 +0530

    example

--- index.php
+++ index.php
@@ -7,16 +7,8 @@ $pos2 = stripos($requested_uri, '/organisation/');
 $pos3 = stripos($requested_uri, '/case-study/');
 $pos4 = stripos($requested_uri, '/profile/');
 
-// removed that line!
 //
-// aawaarapan.....banjaraapann
-// adadad
-// asdadadasdads
-// adadadadasdadsasdad
-// asdadadad
-// ada
-// aawaarapan.....banjaraapanns
-// ssadasdsadasd
+// removed that line!
 //
 if( $pos1 !== false || $pos2 !== false || $pos3 !== false || $pos4 !== false ){ echo "true" ; } else { echo "false" ; }
 
